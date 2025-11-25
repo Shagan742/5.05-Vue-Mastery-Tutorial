b913d8644191c07e353465e8bf4ab9f83867128c
Shagan742 <526ssangha@frhsd.com>
2025-11-25 13:35:35 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app=Vue.createApp({
     data: function() {
         return {
             product:'Socks',
-            description:'they are worn'
+            image:'./assets/images/socks_green.jpg',
+            url:'https://github.com/Shagan742/5.05-Vue-Mastery-Tutorial'
         }
     }
 })
