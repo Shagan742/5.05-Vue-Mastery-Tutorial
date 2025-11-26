621db4b4ac50aef613bd0fd338b9698353fbfad3
Shagan742 <526ssangha@frhsd.com>
2025-11-26 19:55:07 +0000

    Class & Style Binding

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app=Vue.createApp({
             cart:0,
             product:'Socks',
             image:'./assets/images/socks_green.jpg',
-            inStock: true,
+            inStock: false,
             details:['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
@@ -19,9 +19,6 @@ const app=Vue.createApp({
         },
         updateImage(variantImage) {
             this.image=variantImage
-        },
-        removeFromCart() {
-            this.cart-=1
         }
     }
 })
